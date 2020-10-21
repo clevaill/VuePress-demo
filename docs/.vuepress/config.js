81d5d8f9ba37c60258f82c192ce55969443f32c0
Clément Levailant <clement@koji.fr>
2020-10-21 16:39:45 +0200

    example

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+        lastUpdated: 'Last Updated',
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          {
+            text: 'Languages',
+            ariaLabel: 'Language Menu',
+            items: [
+              { text: 'Chinese', link: '/language/chinese/' },
+              { text: 'Japanese', link: '/language/japanese/' }
+            ]
+          },
+          { text: 'External', link: 'https://google.com' }
+        ],
+        sidebar: [
+            {
+              title: 'Group 1',   // required
+              path: '/foo/',      // optional, link of the title, which should be an absolute path and must exist
+              collapsable: false, // optional, defaults to true
+              sidebarDepth: 1,    // optional, defaults to 1
+              children: [
+                '/guide/',
+                '/installation/',
+                '/advanced/'
+              ]
+            }
+          ]
+      
+      }
+}
