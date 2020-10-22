5c6ca9b2903a18d87c6696dd1706facaa79a202e
Clément Levailant <clement@koji.fr>
2020-10-22 16:10:31 +0200

    Documentation

--- docs/.vuepress/oktaConfig.js
+++
@@ -1,6 +0,0 @@
-export const oktaConfig = {
-    issuer: "https://dev-5560942.okta.com/oauth2/default",
-    client_id: "0oadb1h8xLILXWFsI5d5",
-    redirect_uri: "http://localhost:8080/implicit/callback/",
-    scope: "openid profile email"
-};
